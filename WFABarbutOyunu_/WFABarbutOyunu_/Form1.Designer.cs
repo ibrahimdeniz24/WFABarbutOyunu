fd7ab9fe3b4a81fa153f4fcc40fc23cbbde1350e
ibrahimdeniz24 <160463714+ibrahimdeniz24@users.noreply.github.com>
2024-02-19 10:09:18 +0300

    BarbutAtmaOyunu
    BarbutOyunu

---
+++ WFABarbutOyunu_/WFABarbutOyunu_/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WFABarbutOyunu_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKirmiziOyuncuZar = new System.Windows.Forms.Label();
+            this.btnKirmiziOyuncuZarAt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lblMaviOyuncuZar = new System.Windows.Forms.Label();
+            this.btnMaviOyuncuZarAt = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.grpSkorTablosu = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblMaviOyuncuSkor = new System.Windows.Forms.Label();
+            this.lblKirmiziOyuncuSkor = new System.Windows.Forms.Label();
+            this.grpSkorTablosu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblKirmiziOyuncuZar
+            // 
+            this.lblKirmiziOyuncuZar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblKirmiziOyuncuZar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblKirmiziOyuncuZar.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKirmiziOyuncuZar.Location = new System.Drawing.Point(54, 86);
+            this.lblKirmiziOyuncuZar.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblKirmiziOyuncuZar.Name = "lblKirmiziOyuncuZar";
+            this.lblKirmiziOyuncuZar.Size = new System.Drawing.Size(150, 150);
+            this.lblKirmiziOyuncuZar.TabIndex = 0;
+            this.lblKirmiziOyuncuZar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnKirmiziOyuncuZarAt
+            // 
+            this.btnKirmiziOyuncuZarAt.Location = new System.Drawing.Point(54, 256);
+            this.btnKirmiziOyuncuZarAt.Margin = new System.Windows.Forms.Padding(5);
+            this.btnKirmiziOyuncuZarAt.Name = "btnKirmiziOyuncuZarAt";
+            this.btnKirmiziOyuncuZarAt.Size = new System.Drawing.Size(153, 36);
+            this.btnKirmiziOyuncuZarAt.TabIndex = 1;
+            this.btnKirmiziOyuncuZarAt.Text = "Zar At";
+            this.btnKirmiziOyuncuZarAt.UseVisualStyleBackColor = true;
+            this.btnKirmiziOyuncuZarAt.Click += new System.EventHandler(this.btnKirmiziOyuncuZarAt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kırmızı Oyuncu";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSonuc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSonuc.Location = new System.Drawing.Point(54, 318);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(357, 59);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMaviOyuncuZar
+            // 
+            this.lblMaviOyuncuZar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblMaviOyuncuZar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMaviOyuncuZar.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMaviOyuncuZar.Location = new System.Drawing.Point(261, 86);
+            this.lblMaviOyuncuZar.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblMaviOyuncuZar.Name = "lblMaviOyuncuZar";
+            this.lblMaviOyuncuZar.Size = new System.Drawing.Size(150, 150);
+            this.lblMaviOyuncuZar.TabIndex = 0;
+            this.lblMaviOyuncuZar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnMaviOyuncuZarAt
+            // 
+            this.btnMaviOyuncuZarAt.Location = new System.Drawing.Point(261, 256);
+            this.btnMaviOyuncuZarAt.Margin = new System.Windows.Forms.Padding(5);
+            this.btnMaviOyuncuZarAt.Name = "btnMaviOyuncuZarAt";
+            this.btnMaviOyuncuZarAt.Size = new System.Drawing.Size(150, 36);
+            this.btnMaviOyuncuZarAt.TabIndex = 1;
+            this.btnMaviOyuncuZarAt.Text = "Zar At";
+            this.btnMaviOyuncuZarAt.UseVisualStyleBackColor = true;
+            this.btnMaviOyuncuZarAt.Click += new System.EventHandler(this.btnMaviOyuncuZarAt_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(256, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 25);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Mavi Oyuncu";
+            // 
+            // grpSkorTablosu
+            // 
+            this.grpSkorTablosu.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.grpSkorTablosu.Controls.Add(this.label7);
+            this.grpSkorTablosu.Controls.Add(this.label6);
+            this.grpSkorTablosu.Controls.Add(this.lblMaviOyuncuSkor);
+            this.grpSkorTablosu.Controls.Add(this.lblKirmiziOyuncuSkor);
+            this.grpSkorTablosu.Location = new System.Drawing.Point(490, 67);
+            this.grpSkorTablosu.Name = "grpSkorTablosu";
+            this.grpSkorTablosu.Size = new System.Drawing.Size(237, 188);
+            this.grpSkorTablosu.TabIndex = 3;
+            this.grpSkorTablosu.TabStop = false;
+            this.grpSkorTablosu.Text = "Skor Tablosu";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label7.Location = new System.Drawing.Point(25, 126);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 25);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Mavi Oyuncu :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(6, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(171, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Kırmızı Oyuncu :";
+            // 
+            // lblMaviOyuncuSkor
+            // 
+            this.lblMaviOyuncuSkor.AutoSize = true;
+            this.lblMaviOyuncuSkor.Location = new System.Drawing.Point(193, 126);
+            this.lblMaviOyuncuSkor.Name = "lblMaviOyuncuSkor";
+            this.lblMaviOyuncuSkor.Size = new System.Drawing.Size(24, 25);
+            this.lblMaviOyuncuSkor.TabIndex = 2;
+            this.lblMaviOyuncuSkor.Text = "0";
+            // 
+            // lblKirmiziOyuncuSkor
+            // 
+            this.lblKirmiziOyuncuSkor.AutoSize = true;
+            this.lblKirmiziOyuncuSkor.Location = new System.Drawing.Point(193, 66);
+            this.lblKirmiziOyuncuSkor.Name = "lblKirmiziOyuncuSkor";
+            this.lblKirmiziOyuncuSkor.Size = new System.Drawing.Size(24, 25);
+            this.lblKirmiziOyuncuSkor.TabIndex = 2;
+            this.lblKirmiziOyuncuSkor.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 437);
+            this.Controls.Add(this.grpSkorTablosu);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnMaviOyuncuZarAt);
+            this.Controls.Add(this.btnKirmiziOyuncuZarAt);
+            this.Controls.Add(this.lblMaviOyuncuZar);
+            this.Controls.Add(this.lblKirmiziOyuncuZar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpSkorTablosu.ResumeLayout(false);
+            this.grpSkorTablosu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKirmiziOyuncuZar;
+        private System.Windows.Forms.Button btnKirmiziOyuncuZarAt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lblMaviOyuncuZar;
+        private System.Windows.Forms.Button btnMaviOyuncuZarAt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox grpSkorTablosu;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMaviOyuncuSkor;
+        private System.Windows.Forms.Label lblKirmiziOyuncuSkor;
+    }
+}
+
---
+++ WFABarbutOyunu_/WFABarbutOyunu_/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace WFABarbutOyunu_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rnd = new Random();
+        int maviOyuncuSkor = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnMaviOyuncuZarAt.Enabled = false;
+        }
+
+        private void btnKirmiziOyuncuZarAt_Click(object sender, EventArgs e)
+        {
+            int kirmiziOyuncuZar = rnd.Next(1, 7);
+            lblKirmiziOyuncuZar.Text = kirmiziOyuncuZar.ToString();
+            btnKirmiziOyuncuZarAt.Enabled = false;
+            btnMaviOyuncuZarAt.Enabled = true;
+
+            
+            
+        }
+
+        private void btnMaviOyuncuZarAt_Click(object sender, EventArgs e)
+        {
+            int maviOyuncuZar = rnd.Next(1, 7);
+            lblMaviOyuncuZar.Text = maviOyuncuZar.ToString();
+            btnKirmiziOyuncuZarAt.Enabled = true;
+            btnMaviOyuncuZarAt.Enabled = false;
+            
+            int MaviOyuncuZar = Convert.ToInt32(lblMaviOyuncuZar.Text);
+            int KirmiziOyuncuZar = Convert.ToInt32(lblKirmiziOyuncuZar.Text);
+
+            if ( KirmiziOyuncuZar == MaviOyuncuZar)
+            {
+                lblSonuc.Text = "Beraberlik";
+            }
+            else if ( KirmiziOyuncuZar > MaviOyuncuZar)
+            {
+
+                int KirmiziOyuncuSkor = Convert.ToInt32(lblKirmiziOyuncuSkor.Text);
+                KirmiziOyuncuSkor++;
+                lblKirmiziOyuncuSkor.Text = KirmiziOyuncuSkor.ToString();
+
+                lblSonuc.Text = "Kırmızı Oyuncu Kazandı ";
+            }
+            else
+            {
+                maviOyuncuSkor++;
+                lblMaviOyuncuSkor.Text = maviOyuncuSkor.ToString() ;
+                lblSonuc.Text = "Mavi Oyuncu Kazandı";
+            }
+
+        }
+
+
+    }
+
+}
